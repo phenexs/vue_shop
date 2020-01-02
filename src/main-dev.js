bb1387511190cb4bd0accd0f439fcbff15f0f862
phenexs <1830451850@qq.com>
2020-01-02 21:45:53 +0800

    完成了项目打包和优化

--- src/main-dev.js
+++ src/main-dev.js
@@ -12,12 +12,19 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 导入axios
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  Nprogress.start()
+  return config
+})
+axios.interceptors.response.use(config => {
+  Nprogress.done()
   return config
 })
 Vue.prototype.axios = axios
---
+++ src/main-prod.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// import './plugins/element.js'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入全局样式
+import './assets/css/global.css'
+import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import Nprogress from 'nprogress'
+// 导入axios
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  Nprogress.start()
+  return config
+})
+axios.interceptors.response.use(config => {
+  Nprogress.done()
+  return config
+})
+Vue.prototype.axios = axios
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+  var y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
